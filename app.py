88615e8dc26de92095051e41ed9c7418938a7a2c
Q. Price <priceqp@gmail.com>
2020-07-25 14:58:29 -0500

    implement Pet class and greet method

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, request, render_template, redirect, flash, session
-from models import db, connect_db
+from models import db, connect_db, Pet
 
 app = Flask(__name__)
 
 # Congifure and Initialize Database
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///movies_example"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///pet_shop_db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_ECHO"] = True
 app.config['SECRET_KEY'] = "chickens"
--- models.py
+++ models.py
@@ -7,3 +7,24 @@ def connect_db(app):
   db.init_app(app)
 
 # MODELS GO BELOW
+class Pet(db.Model):
+  __tablename__ = "pets"
+
+  def __repr__(self):
+    p = self
+    return f"<Pet id={p.id} name={p.name} species={p.species} hunger={p.hunger} "
+
+  id = db.Column(db.Integer,
+                  primary_key=True,
+                  autoincrement=True)
+
+  name = db.Column(db.String(50),
+                    nullable=False,
+                    unique=True)
+                    
+  species = db.Column(db.String(30), nullable=True)
+
+  hunger = db.Column(db.Integer, nullable=False, default=20)
+
+  def greet(self):
+    return f"Hi, I'm {self.name} the {self.species}."
