8cf37813f65a43ecffe66dace236496f487cc151
Q. Price <priceqp@gmail.com>
2020-07-25 15:54:14 -0500

    implement greet method and Pet class methods

--- models.py
+++ models.py
@@ -10,6 +10,14 @@ def connect_db(app):
 class Pet(db.Model):
   __tablename__ = "pets"
 
+  @classmethod
+  def get_by_species(cls, species):
+    return cls.query.filter_by(species = species).all()
+
+  @classmethod
+  def get_all_hungry(cls):
+    return cls.query.filter(Pet.hunger > 20).all()
+
   def __repr__(self):
     p = self
     return f"<Pet id={p.id} name={p.name} species={p.species} hunger={p.hunger} "
@@ -21,10 +29,15 @@ class Pet(db.Model):
   name = db.Column(db.String(50),
                     nullable=False,
                     unique=True)
-                    
+
   species = db.Column(db.String(30), nullable=True)
 
   hunger = db.Column(db.Integer, nullable=False, default=20)
 
   def greet(self):
     return f"Hi, I'm {self.name} the {self.species}."
+
+  def feed(self, amt = 20):
+    """Update hunger based off of amt"""
+    self.hunger -= amt
+    self.hunger = max(self.hunger, 0)
