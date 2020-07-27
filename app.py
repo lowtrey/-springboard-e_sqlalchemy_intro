2f25265c3776e7aaaaf27aa8f2aed372ff4c80eb
Q. Price <priceqp@gmail.com>
2020-07-27 08:16:41 -0500

    implement pet creation form

--- app.py
+++ app.py
@@ -17,6 +17,19 @@ def list_pets():
   pets = Pet.query.all()
   return render_template("list.html", pets=pets)
 
+@app.route("/", methods=["POST"])
+def create_pet():
+  name = request.form["name"]
+  species = request.form["species"]
+  hunger = request.form["hunger"]
+  hunger = int(hunger) if hunger else None
+
+  new_pet = Pet(name=name, species=species, hunger=hunger)
+  db.session.add(new_pet)
+  db.session.commit()
+
+  return redirect(f"/{new_pet.id}")
+
 @app.route("/<int:pet_id>")
 def show_pet(pet_id):
   """Show details about a single pet"""
